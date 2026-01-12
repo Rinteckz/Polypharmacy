c7eac638631a02167c349a8d04990a1a77bc3be3
Rinteckz <edalmaco10@gmail.com>
2026-01-12 08:41:05 -0600

    Fix typo in metric display for Total Interactions

--- farmacos.py
+++ farmacos.py
@@ -576,7 +576,7 @@ def pestaña_visualizacion():
             with col1:
                 st.metric("Total Drugs in Dataset", len(G_completo.nodes()))
             with col2:
-                st.mwtric("Total Interactions","191808")
+                st.metric("Total Interactions","191808")
             
             category_counts = Counter()
             for node in G_completo.nodes():
